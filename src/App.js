cd0675135370060ddb7d1f030b6f53e143403d7e
websivustot <websivustot@gmail.com>
2018-11-16 11:12:50 +0200

    the project Health & Fit is created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Logo from './logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src={Logo} alt="Health & Fit" className="logo"/>
+        <h1>Welcome to us!</h1>
+        <p>This app helps you to be healthier and fit.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
